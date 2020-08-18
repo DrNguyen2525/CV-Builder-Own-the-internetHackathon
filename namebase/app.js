259c82aacada9e6192604e0bd885f636e5a35593
hoang <vinhyenvodoi98@gmail.com>
2020-08-18 23:43:10 +0700

    feat : upload avatar

--- namebase/app.js
+++ namebase/app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 var cors = require('cors');
-
+var fs = require('fs');
 var app = express();
 
 // handshake login
@@ -27,6 +29,22 @@ app.use(cookieParser());
 
 // Route
 app.use('/', route);
+
+// Upload avatar
+app.post('/avatar', upload.single('avatar'), (req, res) => {
+  const processedFile = req.file || {}; // MULTER xử lý và gắn đối tượng FILE vào req
+  let orgName = processedFile.originalname || ''; // Tên gốc trong máy tính của người upload
+  orgName = orgName.trim().replace(/ /g, '-');
+  const fullPathInServ = processedFile.path; // Đường dẫn đầy đủ của file vừa đc upload lên server
+  // Đổi tên của file vừa upload lên, vì multer đang đặt default ko có đuôi file
+  const newFullPath = `${fullPathInServ}-${orgName}`;
+  fs.renameSync(fullPathInServ, newFullPath);
+  res.send({
+    status: true,
+    message: 'file uploaded',
+    fileNameInServer: newFullPath
+  });
+});
 
 app.listen(4000, (err) => {
   console.log('Server is running in port 4000 !!!');
