0b7a7bb9c76624e87e7140e383bccd995171707b
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-08-20 18:03:53 +0700

    feat: add server source code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const multer = require('multer');
+const fs = require('fs');
+var skynet = require('@nebulous/skynet');
+
+const upload = multer({ dest: 'uploads/' });
 
 const { api, getPdfFromPage } = require('../helper');
 
@@ -15,7 +20,7 @@ router.get('/connecting', async (req, res) => {
     });
   } catch (error) {
     res.status(400).json({
-      err
+      error
     });
   }
 });
@@ -30,9 +35,45 @@ router.post('/upload', async (req, res) => {
       response
     });
   } catch (error) {
-    res.status(400).json({
-      response
+    res.status(500).json({ error });
+  }
+});
+
+// Upload avatar
+router.post('/avatar', upload.single('avatar'), async (req, res) => {
+  try {
+    // MULTER process and assign FILE object to req
+    const processedFile = req.file || {};
+
+    // Uploaded file's original name
+    let orgName = processedFile.originalname || '';
+    orgName = orgName.trim().replace(/ /g, '-');
+
+    // Full path of uploaded file on server
+    const fullPathInServer = processedFile.path;
+
+    // Add orignal file extension, as multer name the file without extension by default
+    const newFullPath = `${fullPathInServer}.${orgName.split('.').slice(1).pop() || ''}`;
+
+    fs.renameSync(fullPathInServer, newFullPath);
+
+    let skylink = await skynet.uploadFile(`./${newFullPath}`);
+
+    fs.unlink(`./${newFullPath}`, error => {
+      if (error) throw error;
     });
+
+    skylink = skylink.toString().slice(6);
+
+    res.json({
+      status: true,
+      message: 'Photo uploaded successfully.',
+      skylink
+    });
+  } catch (error) {
+    if (error) throw error;
+
+    res.status(500).json(error);
   }
 });
 
@@ -74,9 +115,24 @@ router.post('/uploadToSkynet', async (req, res) => {
     });
 
   try {
-    await getPdfFromPage(`${BASE_URL}/r/${id}`, type, `./uploads/SiabaseCV-${id}.pdf`);
+    // File location on server
+    const path = `./uploads/SiabaseCV-${id}.pdf`;
 
-    res.json({ message: 'Uploaded successfully.' });
+    await getPdfFromPage(`${BASE_URL}/r/${id}`, type, path);
+
+    let skylink = await skynet.uploadFile(path);
+
+    fs.unlink(path, error => {
+      if (error) throw error;
+    });
+
+    skylink = skylink.toString().slice(6);
+
+    res.json({
+      status: true,
+      message: 'Resume uploaded successfully.',
+      skylink
+    });
   } catch (error) {
     res.status(500).json(error);
   }
